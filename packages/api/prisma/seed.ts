c099ad91f880dfcbb172b22f7e61704d4a9a70a2
Eser Ozvataf <eser@ozvataf.com>
2023-06-25 23:30:12 +0300

    prisma schema (take V).

--- packages/api/prisma/seed.ts
+++ packages/api/prisma/seed.ts
@@ -3,25 +3,15 @@ import { PrismaClient } from ".prisma/client/edge";
 const main = async () => {
   const prisma = new PrismaClient();
 
-  const eser = await prisma.user.upsert({
+  const userEser = await prisma.user.upsert({
     where: { email: "eser@ozvataf.com" },
     update: {},
     create: {
-      slug: "eser",
       email: "eser@ozvataf.com",
-      name: "Eser",
-      posts: {
-        create: {
-          slug: "hello-world",
-          title: "Hello world!",
-          content: "https://acikyazilimagi.com/",
-          publishedAt: new Date(),
-        },
-      },
     },
   });
 
-  console.log(eser);
+  console.log(userEser);
 
   await prisma.$disconnect();
 };
