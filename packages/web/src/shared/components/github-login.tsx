e582f42d1ea1f935b2baa5bf2669e3db83c85590
Eser Ozvataf <eser@ozvataf.com>
2023-06-26 16:54:29 +0300

    fixed pipeline building issue by temporarily disabling non-complete github login feature.

--- packages/web/src/shared/components/github-login.tsx
+++ packages/web/src/shared/components/github-login.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 
 import { Button } from "@/shared/components/ui/button.tsx";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const signInWithGitHub = () => {
-  // TODO(@eser) implement github login
-};
+// // eslint-disable-next-line @typescript-eslint/no-empty-function
+// const signInWithGitHub = () => {
+//   // TODO(@eser) implement github login
+// };
 
 const GitHubLogin = () => {
   return (
     <Button
-      onClick={() => signInWithGitHub()}
+      // onClick={() => signInWithGitHub()}
       className="flex items-center justify-center gap-2"
     >
       <span>Sign in w/ GitHub</span>
