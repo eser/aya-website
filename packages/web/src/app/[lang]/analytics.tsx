9287f4a75b281340bf2cbe552cccb75b3bfe113c
Eser Ozvataf <eser@ozvataf.com>
2023-06-27 21:38:53 +0300

    temporarily disabled vercel analytics.

--- packages/web/src/app/[lang]/analytics.tsx
+++ packages/web/src/app/[lang]/analytics.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+// import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 const Analytics = () => {
-  return <VercelAnalytics />;
+  // return <VercelAnalytics />;
+  return null;
 };
 
 export { Analytics, Analytics as default };
