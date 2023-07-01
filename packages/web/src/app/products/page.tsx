093640d5b137241684f69ec06b96aac63bf12ec3
Eser Ozvataf <eser@ozvataf.com>
2023-07-01 04:12:45 +0300

    content organization (take II).

--- packages/web/src/app/products/page.tsx
+++ packages/web/src/app/products/page.tsx
@@ -37,7 +37,7 @@ const ProductsPage = (/* props: ProductsPageProps */) => {
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Projeler
+            Üretimler
           </h1>
           <div className="max-w-[980px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
             Sayfa henüz hazırlık aşamasında.
