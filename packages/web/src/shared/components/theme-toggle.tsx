d84229112f3900561125007a2da498bdaeb25a39
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-05 22:34:00 +0300

    fix: translate texts to Turkish

--- packages/web/src/shared/components/theme-toggle.tsx
+++ packages/web/src/shared/components/theme-toggle.tsx
@@ -21,21 +21,21 @@ const ThemeToggle = () => {
         <Button variant="ghost" size="sm">
           <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Temayı değiştir</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" forceMount>
         <DropdownMenuItem onClick={() => setTheme("light")}>
           <Icons.sun className="mr-2 h-4 w-4" />
-          <span>Light</span>
+          <span>Açık</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           <Icons.moon className="mr-2 h-4 w-4" />
-          <span>Dark</span>
+          <span>Koyu</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           <Icons.laptop className="mr-2 h-4 w-4" />
-          <span>System</span>
+          <span>Sistem</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
