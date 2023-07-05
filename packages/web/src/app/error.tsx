b008239fa3832107233985d6caa12296bf20ad22
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-05 22:20:10 +0300

    fix: update error page layout and text

--- packages/web/src/app/error.tsx
+++ packages/web/src/app/error.tsx
@@ -1,6 +1,8 @@
 "use client"; // Error components must be Client components
 
 import { useEffect } from "react";
+import { Layout } from "@/shared/components/layout.tsx";
+import { Button } from "@/shared/components/ui/button.tsx";
 
 const Error = ({
   error,
@@ -15,19 +17,31 @@ const Error = ({
     console.error(error);
   }, [error]);
 
+  const placeholders: Record<string, string> = {
+    // lang: props.params.lang,
+  };
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        type="button"
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <Layout placeholders={placeholders}>
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+        <div className="flex max-w-[980px] flex-col items-start gap-2">
+          <h1>
+            Bir hata oluştu
+          </h1>
+
+          <div className="max-w-[980px] text-lg sm:text-xl">
+            <Button
+              onClick={
+                // Attempt to recover by trying to re-render the segment
+                () => reset()
+              }
+            >
+              Yeniden dene
+            </Button>
+          </div>
+        </div>
+      </section>
+    </Layout>
   );
 };
 
