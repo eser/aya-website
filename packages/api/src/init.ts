c4152f3716ecf087c34038633dfec5b651701674
Zekeriya Koc <zekzekus@gmail.com>
2023-07-24 21:47:08 +0300

    Fix file path to stop deno lsp whining about it

--- packages/api/src/init.ts
+++ packages/api/src/init.ts
@@ -1,7 +1,7 @@
 import { loadEnv } from "@hex/lib/options/env.ts";
 import {
   getSupabaseClient,
-} from "./supabase/functions/_shared/supabase-client.ts";
+} from "../supabase/functions/_shared/supabase-client.ts";
 import { profileCreate } from "./supabase/functions/profile-create/mod.ts";
 import { profileGet } from "./supabase/functions/profile-get/mod.ts";
 import { profileRemove } from "./supabase/functions/profile-remove/mod.ts";
