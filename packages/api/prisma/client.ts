9859b318ff111f2bf4b46ef719b8599f596bf76b
Eser Ozvataf <eser@ozvataf.com>
2023-07-26 20:06:28 +0300

    added DATABASE_ON_EDGE env value.

--- packages/api/prisma/client.ts
+++ packages/api/prisma/client.ts
@@ -1,19 +1,23 @@
 import * as Regular from ".prisma/client";
 import * as Edge from ".prisma/client/edge";
 
-const getDatabaseUrl = () => {
+const getDatabaseConfiguration = () => {
   if (typeof Deno !== "undefined") {
-    return Deno.env.get("DATABASE_URL");
+    return [
+      Deno.env.get("DATABASE_URL"),
+      Deno.env.get("DATABASE_ON_EDGE")?.startsWith("prisma://") ?? false,
+    ];
   }
 
-  return process.env.DATABASE_URL;
+  return [
+    process.env.DATABASE_URL,
+    process.env.DATABASE_ON_EDGE?.startsWith("prisma://") ?? false,
+  ];
 };
 
 const getPrismaClient = () => {
-  const databaseUrl = getDatabaseUrl();
-  const isEdge = databaseUrl.startsWith("prisma://");
-
-  const PrismaClient = isEdge ? Edge.PrismaClient : Regular.PrismaClient;
+  const [databaseUrl, isOnEdge] = getDatabaseConfiguration();
+  const PrismaClient = isOnEdge ? Edge.PrismaClient : Regular.PrismaClient;
 
   const prisma = new PrismaClient({
     datasources: {
